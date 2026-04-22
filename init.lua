22f2974a6adf6f41210807e799a3767589d4044c
Juha <juhaherrera@gmail.com>
2026-04-22 14:17:11 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-minetest.register_node("bloque_simple:bloque_simple", {
+minetest.register_node("bloque_simple", {
 	description = "Bloque Simple",
 	tiles = {"bloque_simple.png"},
 	is_ground_content = false,
